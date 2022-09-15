861ef2a65fe881d8f4b5a724aea471460cc2e010
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-09-14 22:06:17 -0500

    front end

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -14,7 +15,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+//maybe inside of  startApollo???????
+//serve up static assets
+if(process.env.NODE_ENV ==='production'){
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 //create a new instance of an apollo server with graphql schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
